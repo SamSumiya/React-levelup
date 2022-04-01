1ae1e96e20ad50be7c2ae4a008251c2168f8befb
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-04-01 12:59:33 -0700

    finished uncontrolledform

--- controlled-uncontrolled-comp/src/App.js
+++ controlled-uncontrolled-comp/src/App.js
@@ -1,25 +1,51 @@
 import './App.css';
-import { useState } from 'react'
-import { UncontrolledForm } from './UncontrolledForm'
-import { ControlledForm } from './ControlledForm'
-import { ControlledModal } from './ControlledModal';
+import { useState } from 'react';
+// import { UncontrolledForm } from './UncontrolledForm'
+// import { ControlledForm } from './ControlledForm'
+// import { ControlledModal } from './ControlledModal';
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 
 function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
 
-  const [shouldShowModal, setShouldShowModal] = useState(false)
+
+  const StepOne = ({ goToNext }) => (
+    <>
+      <h2>Step 1</h2>
+      <button onClick={() => goToNext({name: 'Alex'})}>Next</button>
+    </>
+  );
+  const StepTwo = ({ goToNext }) => (
+    <>
+      <h2>Step 2</h2>
+      <button onClick={() => goToNext({ age: 22 })}>Next</button>
+    </>
+  );
+  const StepThree = ({ goToNext }) => (
+    <>
+      <h2>Step 3</h2>
+      <button onClick={() => goToNext({hairColor: 'Red'})}>Next</button>
+    </>
+  );
 
   return (
     <>
       {/* <UncontrolledForm /> */}
       {/* <ControlledForm /> */}
-      <ControlledModal shouldShow={shouldShowModal}
+      {/* <ControlledModal shouldShow={shouldShowModal}
         onRequestClose={() => setShouldShowModal(false)}>
           <h1>Hello</h1>
       </ControlledModal>
       <button onClick={() => setShouldShowModal(!shouldShowModal)}>
         {shouldShowModal ? 'Hide' : 'Show'}
-      </button>
+      </button> */}
       {/* </ControlledModal> */}
+
+      <UncontrolledOnboardingFlow onFinish={data => console.log(data)}>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledOnboardingFlow>
     </>
   );
 }
---
+++ controlled-uncontrolled-comp/src/UncontrolledOnboardingFlow.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
+
+  const [onbordingData, setOnboardingData] = useState({})
+  const [currentIdx, setCurrentIdx] = useState(0)
+
+  const currentChild = React.Children.toArray(children)[currentIdx]
+
+  const goToNext = (stepData) => {
+    const nextIndex = currentIdx + 1 
+    const updatedData = {
+      ...onbordingData, 
+      ...stepData, 
+    }
+
+    console.log(updatedData);
+    if (nextIndex < children.length) {
+      setCurrentIdx(nextIndex);
+    } else {
+      onFinish(updatedData);
+    }
+    setOnboardingData(updatedData)
+  }
+
+  if (React.isValidElement(currentChild)) {
+    return React.cloneElement(currentChild, { goToNext })
+  }
+
+  return currentChild
+}
